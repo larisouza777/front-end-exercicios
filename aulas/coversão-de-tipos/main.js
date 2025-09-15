3dd74da89ca7c2a8c1b1dfce72fe892d8a0664af
larisouza777 <larissa.ygui@gmail.com>
2025-09-15 20:03:42 -0300

    Exercicio 1 - 2

---
+++ aulas/coversão-de-tipos/main.js
@@ -0,0 +1,32 @@
+// EXEMPLO 
+//let Marca = "Sansung";
+//let Modelo = "Galaxy S10";
+
+
+// if (Marca == 'Sansung') {
+
+//     console.log ('Voce tem 20% de desconto.');
+// } else {
+//     console.log ('Voce nao tem desconto.');
+// }
+
+// Marca = prompt ('Insira a marca de seu aparelho')
+
+//EXERCICIO NUMERO 1 
+// let Numero = prompt ('Insira um numero');
+
+// if (Numero >10) {
+//     console.log ('O numero e maior que 10');
+// } else { 
+//     console.log ('O numero e menor que 10');
+// }
+//EXERCICIO NUMERO 2
+// let Numero = prompt ('Insira um numero');
+
+// if (Numero >532 && Numero <743) {
+//     console.log ('O numero e maior que 532 e menor que 743');
+
+// } else  { 
+//     console.log ('O seu numero nao esta entre 532 e 743');
+// }
+
