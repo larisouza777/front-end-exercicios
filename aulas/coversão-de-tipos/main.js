2634a824b9e094ef544261dcd3024801230aec3e
larisouza777 <larissa.ygui@gmail.com>
2025-09-15 21:50:39 -0300

    Reformulando exercicios

--- aulas/coversão-de-tipos/main.js
+++ aulas/coversão-de-tipos/main.js
@@ -12,7 +12,7 @@
 
 // Marca = prompt ('Insira a marca de seu aparelho')
 
-//EXERCICIO NUMERO 1 
+// EXERCICIO NUMERO 1 
 // let Numero = prompt ('Insira um numero');
 
 // if (Numero >10) {
@@ -20,20 +20,24 @@
 // } else { 
 //     console.log ('O numero e menor que 10');
 // }
-//EXERCICIO NUMERO 2
-// let Numero = prompt ('Insira um numero');
+// EXERCICIO NUMERO 2
+// let x = prompt ('Insira um numero');
 
-// if (Numero >532 && Numero <743) {
-//     console.log ('O numero e maior que 532 e menor que 743');
+// console.log(typeof x);
+
+// if (x >532 && x <743) {
+//     console.log ('O numero esta entre 532 e 743');
 
 // } else  { 
 //     console.log ('O seu numero nao esta entre 532 e 743');
 // }
 
-//EXERCICIO SENHA 
-// let Senha = prompt ('Insira sua Senha');
+// //EXERCICIO SENHA 
 
-// if (Senha == 'pipoca') {
+// let Senha = prompt ('Insira sua Senha');
+// let senhaArmazenada = 'pipoca'; // camel-case
+
+// if (Senha == senhaArmazenada) {
 //     console.log ('Acesso Permitido');
 // } else {
 //     console.log ('Acesso Negado');
@@ -41,11 +45,39 @@
 
 //EXERCICO IDADE E SALARIO
 // let Idade = prompt ('Insira sua Idade');
-// let Salario = prompt ('Insira seu Salario');
+
 
 // if (Idade > 18 && Salario >= 2000) {
+
+//     let Salario = prompt ('Insira seu Salario');
 //     console.log ('Voce pode fazer o emprestimo');
 // } else {
 //     console.log ('Voce nao pode fazer o emprestimo');
 // }
 
+// if (Idade >= 18) {
+//     if (Salario >= 2000) {
+//     }
+// } else {
+//     console.log ('Infelizmente voce nao tem idade para fazer o emprestimo');
+// }
+
+let experiencia = prompt ('Insira sua experiencia em anos');
+let pontuacao = 0
+
+if (experiencia <= 5) {
+    pontuacao = pontuacao + 10;
+} else if (experiencia >= 5 ) {
+    pontuacao = pontuacao + 20;
+} 
+
+let formacao = prompt ('Insira sua formacao');
+
+if (formacao == 'graduacao') {
+    pontuacao = pontuacao + 20;
+}   else if (formacao == 'mestrado') {
+    pontuacao = pontuacao + 30;
+} else if (formacao == 'doutorado') {
+    pontuacao = pontuacao + 40;
+}
+console.log ( `Sua pontuacao total e de ${pontuacao} pontos`);
