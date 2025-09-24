99583584990629a6e08c294e6b24c3b930a79633
larisouza777 <larissa.ygui@gmail.com>
2025-09-24 20:52:19 -0300

    exercico "Array"

--- exercicios/funcão/main.js
+++ exercicios/funcão/main.js
@@ -44,8 +44,9 @@
 // minhaFuncao(meucarro);
 // y = meucarro.make;
 
-var fatorial = function fac(n) {
-  return n < 2 ? 1 : n * fac(n - 1);
-};
+// var fatorial = function fac(n) {
+//   return n < 2 ? 1 : n * fac(n - 1);
+// };
 
-console.log(fatorial(3));
+// console.log(fatorial(3));
+
