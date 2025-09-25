3b1ce60bc7cb97229fa68d1ced3328307ff050b0
larisouza777 <larissa.ygui@gmail.com>
2025-09-25 20:57:55 -0300

    alterações

--- exercicios/array/array.js
+++ exercicios/array/array.js
@@ -1,32 +1,29 @@
-let usoDiaDaSemana = [1, 2, 2.4, 5, 1.2, 0.5, 12]; // Domingo, Segunda etc...
-let diasDaSemana = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira','Sexta-Feira', 'Sábado']
+// let usoDiaDaSemana = [1, 2, 2.4, 5, 1.2, 0.5, 12]; // Domingo, Segunda etc...
+// let diasDaSemana = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira','Sexta-Feira', 'Sábado']
 
-// Determinar se o uso do dia é Verde, Amarelo ou Vermelho
-const indicadorDeUso = (numeroDeHoras) => {
-    if (numeroDeHoras <= 1) {
-        return '💚';
-    } else if (numeroDeHoras <= 3) {
-        return '💛';
-    } else {
-        return '❤️';
-    }
-}
+// // Determinar se o uso do dia é Verde, Amarelo ou Vermelho
+// const indicadorDeUso = (numeroDeHoras) => {
+//     if (numeroDeHoras <= 1) {
+//         return '💚';
+//     } else if (numeroDeHoras <= 3) {
+//         return '💛';
+//     } else {
+//         return '❤️';
+//     }
+// }
 
-for(let contador = 0; contador <= 6; contador++) {
-    let farol=indicadorDeUso(usoDiaDaSemana[contador]);
-    let dia = diasDaSemana[contador];
-    let horas = usoDiaDaSemana[contador];
-    let mensagem = 'horas de uso';
+// for(let contador = 0; contador <= 6; contador++) {
+//     let farol=indicadorDeUso(usoDiaDaSemana[contador]);
+//     let dia = diasDaSemana[contador];
+//     let horas = usoDiaDaSemana[contador];
+//     let mensagem = 'horas de uso';
 
-if (horas > 1) {
-        mensagem = 'horas de uso';
-}
+// if (horas > 1) {
+//         mensagem = 'horas de uso';
+// }
 
-    console.log(`${farol} 🗓️ ${dia} | ${horas} horas de uso`);
-}
-
-
-
+//     console.log(`${farol} 🗓️ ${dia} | ${horas} horas de uso`);
+// }
 
 // console.log ( 'Domingo:' + indicadorDeUso (usoDiaDaSemana [0]));
 // console.log ( 'Segunda-Feira:' + indicadorDeUso (usoDiaDaSemana [1]));
@@ -41,3 +38,8 @@ if (horas > 1) {
 // console.log(typeof indicadorDeUso); // funcion 
 // diaDaSemana[0] 
 
+let objetoHTML = document.getElementById('title');
+
+objetoHTML.innerText = 'Desafio fácil';
+objetoHTML.innerHTML = 'Desafio <b> Facil </b>';
+objeto
